e51ec869c765b0d4b42e85c8689f543e301f7d40
Magnus Hansen <magnus131hansen@gmail.com>
2025-05-22 14:56:17 +0200

    Removed guildName from migration

---
+++ Api/Migrations/20250522125406_Removed_guildName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removed_guildName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GuildName",
+                table: "Activities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GuildName",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
